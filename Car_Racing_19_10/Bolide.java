b25a260c1ed8b269788839cb03d4bc53d3a8b488
Munir <gm.BlackGod@gmail.com>
2015-10-23 15:53:09 +0300

    1910

--- Car_Racing_19_10/Bolide.java
+++ Car_Racing_19_10/Bolide.java
@@ -1,30 +1,8 @@
 
-public class Bolide extends Racing_Car implements Nitrogen_Injection{
-
-	@Override
-	void toRide() {
-		// TODO Auto-generated method stub	
-	}
-
-	@Override
-	void slowDown() {
-		// TODO Auto-generated method stub		
-	}
-
-	@Override
-	void turnLeft() {
-		// TODO Auto-generated method stub		
-	}
-
-	@Override
-	void turnRight() {
-		// TODO Auto-generated method stub		
-	}
+public class Bolide extends Racing_Car {
 
 	
-	public void nitrogenInjection() {
-		// TODO Auto-generated method stub
-		System.out.println("Впрыск азота");
-	}
+	
+	
 
 }
--- Car_Racing_19_10/Rally_Car.java
+++ Car_Racing_19_10/Rally_Car.java
@@ -1,28 +1,6 @@
 
 public class Rally_Car extends Racing_Car{
 
-	@Override
-	void toRide() {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	void slowDown() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	void turnLeft() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	void turnRight() {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
